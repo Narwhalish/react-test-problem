1f5e51216557fcb5b4b34d83e64ee38e5bf41301
Narwhalish <bryao@seas.upenn.edu>
2022-09-28 21:03:14 -0400

    Add tests for Default and Clicked button stories.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,18 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from "@testing-library/react";
 
-// import your story from App.stories.js
-
+import { Default, Clicked } from "./App.stories";
 
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+
+it("Render App with Default Button.", () => {
+  render(<Default />);
+  expect(screen.getByText(/click me/i)).not.toBeNull();
+});
+
+it("Click Default Button to Clicked Button state.", () => {
+  render(<Default />);
+  expect(screen.getByText(/click me/i)).not.toBeNull();
+  fireEvent.click(screen.getByText(/click me/i));
+  expect(screen.getByText(/thanks/i)).not.toBeNull();
+});
